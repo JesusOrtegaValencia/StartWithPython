0a2d035a8074a55335db454d2702f3c1841f21b7
Jesus Maximiliano Ortega Valencia <ortegajesusmaximiliano@gmail.com>
2026-05-02 18:32:55 -0600

    Delete tables.py

--- tables.py
+++
@@ -1,8 +0,0 @@
-from main import Base
-from sqlalchemy import Column, Integer, String, UniqueConstraint
-
-class Cliente(Base):
-    __tablename__ = "clientes"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    email = Column(String(100), unique=True, nullable=False)
